5f796eaf78f0e54f50d4a6b4cad6a9fa619e1bbd
Joe Beda <joe.github@bedafamily.com>
2017-02-01 18:24:50 -0800

    Fixes networking for Docker 1.13

--- tf-scripts/set-iptables.sh
+++ tf-scripts/set-iptables.sh
@@ -16,3 +16,7 @@
 
 # All traffic that isn't to the internal network should be SNAT'd so it can egress
 iptables --append POSTROUTING --table nat -m addrtype ! --dst-type LOCAL ! -d ${cidr} -j MASQUERADE
+
+# Docker 1.13+ will set the FORWARD chain to DROP by default.  Make sure we allow cni0 through.
+iptables -A FORWARD -i cni0 -j ACCEPT
+iptables -A FORWARD -o cni0 -j ACCEPT
